c2432a2c0ef4abfd0824a75493ce812da87976bc
benel-del <sizca0sueka@gmail.com>
2021-12-09 00:02:41 +0900

    react-native-rename: StudyTimer >> SceduleTimer

--- README.md
+++ README.md
@@ -1,4 +1,2 @@
 # ScheduleTimer
  8학기 모바일소프트웨어 강의 프로젝트
-
-StudyTimer >> ScheduleTimer
--- ios/ScheduleTimerTests/ScheduleTimerTests.m
+++ ios/ScheduleTimerTests/ScheduleTimerTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface StudyTimerTests : XCTestCase
+@interface ScheduleTimerTests : XCTestCase
 
 @end
 
-@implementation StudyTimerTests
+@implementation ScheduleTimerTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
